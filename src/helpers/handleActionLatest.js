21aadc33367c17868dfb3a31b70774fbad4494d2
Krasimir Tsonev <krasimir@outset.ws>
2017-11-14 15:11:57 +0200

    Removing a comment

--- src/helpers/handleActionLatest.js
+++ src/helpers/handleActionLatest.js
@@ -1,27 +1,3 @@
-/*
-function test (n) {
-  return {
-    type: 'TEST',
-    n
-  };
-}
-store.runSaga(function * () {
-  yield takeLatest('TEST', function * ({ n }) {
-    console.log(n);
-    console.log('promise: ' + (yield call(a, n)));
-  });
-});
-store.runSaga(function *() {
-  yield put(test(1));
-  yield put(test(2));
-});
-
-1 <-- immediately
-2 <-- immediately
-promise 2 <-- a second later
-*/
-
-
 import handleAction from './handleAction';
 
 const actions = {};
