3ca3b510ad369c8f7c6552f59759773a1d639ac3
Krasimir Tsonev <krasimir@outset.ws>
2017-12-08 06:15:34 +0200

    Updating the example to use stent-dev-tools-emitters

--- examples/todo-app/src/index.js
+++ examples/todo-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Machine } from 'stent';
-import { DevTools } from 'stent/lib/middlewares';
+import StentEmitter from 'stent-dev-tools-emitters/lib/StentEmitter';
 import ToDos from './machines/ToDos.js';
 import App from './components/App';
 import './index.css';
@@ -10,7 +10,7 @@ import './index.css';
 //   console.log(event.data);
 // });
 
-Machine.addMiddleware(DevTools);
+Machine.addMiddleware(StentEmitter);
 Machine.create('ToDos', ToDos);
 
 render(<App />, document.getElementById('root'));
