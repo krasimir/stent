d599556689eea8f5c4eac8dc252347590aa743cd
Krasimir Tsonev <krasimir@outset.ws>
2017-12-12 07:09:09 +0200

    Updating example

--- examples/todo-app/src/index.js
+++ examples/todo-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Machine } from 'stent';
-import StentEmitter from 'stent-dev-tools-emitters/lib/StentEmitter';
+import StentEmitter from 'kuker-emitters/lib/StentEmitter';
 import ToDos from './machines/ToDos.js';
 import App from './components/App';
 import './index.css';
